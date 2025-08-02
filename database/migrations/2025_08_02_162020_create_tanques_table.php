f159011d66f1b2a2f32c89e04206e139b111e53b
Futurama-Tires <programadorfuturamatires@gmail.com>
2025-08-02 10:39:04 -0600

    Creación y Migración de la tabla Tanques

--- database/migrations/2025_08_02_162020_create_tanques_table.php
+++ database/migrations/2025_08_02_162020_create_tanques_table.php
@@ -13,7 +13,19 @@ return new class extends Migration
     {
         Schema::create('tanques', function (Blueprint $table) {
             $table->id();
+
+            $table->foreignId('vehiculo_id')->constrained('vehiculos')->onDelete('cascade');
+
+            $table->unsignedTinyInteger('numero_tanque')->default(1); // Ej. 1, 2, etc.
+            $table->float('capacidad_litros', 8, 2);                  // Capacidad individual del tanque
+            $table->float('rendimiento_estimado', 8, 2)->nullable(); // Manual
+            $table->float('costo_tanque_lleno', 10, 2)->nullable();  // Aprox. si se llena completo
+            $table->enum('tipo_combustible', ['Magna', 'Diesel', 'Premium']);
+
             $table->timestamps();
+
+            // Previene duplicación de número de tanque por vehículo
+            $table->unique(['vehiculo_id', 'numero_tanque']);
         });
     }
 
