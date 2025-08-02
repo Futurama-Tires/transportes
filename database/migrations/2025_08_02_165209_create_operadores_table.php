034a84147a8442bb192453fa4eaf312ae368d0f5
Futurama-Tires <programadorfuturamatires@gmail.com>
2025-08-02 10:54:41 -0600

    Creacion y migrate de operadores

---
+++ database/migrations/2025_08_02_165209_create_operadores_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('operadores', function (Blueprint $table) {
+            $table->id();
+            // Relación con users
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+
+            // Nombre completo
+            $table->string('nombre');
+            $table->string('apellido_paterno');
+            $table->string('apellido_materno')->nullable();
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('operadores');
+    }
+};
